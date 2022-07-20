0a2942849e741fb82b0af0a378216de2eb48eb99
RajeshwarBoyina <rajeshwarboyina@gmail.com>
2022-07-20 18:25:57 +0000

    added newline to check sync between orgin and branch

--- vyoma.sh
+++ vyoma.sh
@@ -7,6 +7,7 @@ pyenv global 3.8.10
 pip install --upgrade pip
 pip install cocotb
 clear
+
 echo "****** UpTickPro (Evaluation Version) 1.0.0 *******"
 echo "Copyright (c) 2022, Vyoma Systems Private Limited"
 echo "All Rights Reserved."
