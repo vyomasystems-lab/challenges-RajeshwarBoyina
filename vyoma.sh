39524bf0b105d523d6616d316a4e9a0a404a4175
RajeshwarBoyina <rajeshwarboyina@gmail.com>
2022-07-23 17:59:44 +0000

    added eof

--- vyoma.sh
+++ vyoma.sh
@@ -13,4 +13,4 @@ rm -rf core.*
 clear
 echo "****** UpTickPro (Evaluation Version) 1.0.0 *******"
 echo "Copyright (c) 2022, Vyoma Systems Private Limited"
-echo "All Rights Reserved.
+echo "All Rights Reserved."
