fbdb936b6148b87640d4462717f01a61d13d4c5e
RajeshwarBoyina <rajeshwarboyina@gmail.com>
2022-08-01 17:27:10 +0000

    modified level2 design

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -8,6 +8,7 @@ from cocotb.decorators import coroutine
 from cocotb.triggers import Timer, RisingEdge
 from cocotb.result import TestFailure
 from cocotb.clock import Clock
+import secrets as sc
 
 from model_mkbitmanip import *
 
@@ -32,31 +33,37 @@ def run_test(dut):
     yield Timer(10) 
     dut.RST_N.value <= 1
 
-    ######### CTB : Modify the test to expose the bug #############
-    # input transaction
-    mav_putvalue_src1 = 0x5
-    mav_putvalue_src2 = 0x0
-    mav_putvalue_src3 = 0x0
-    mav_putvalue_instr = 0x101010B3
+    for i in range(100):
+        ######### CTB : Modify the test to expose the bug #############
+        # input transaction
+        mav_putvalue_src1 = int(sc.token_hex(4),16)
+        mav_putvalue_src2 = int(sc.token_hex(4),16)
+        mav_putvalue_src3 = 0x04
+        mav_putvalue_instr = 0x101010B3
+        print(bin(mav_putvalue_src1))
+        print(bin(mav_putvalue_src3))
 
-    # expected output from the model
-    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+        # expected output from the model
+        expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
 
-    # driving the input transaction
-    dut.mav_putvalue_src1.value = mav_putvalue_src1
-    dut.mav_putvalue_src2.value = mav_putvalue_src2
-    dut.mav_putvalue_src3.value = mav_putvalue_src3
-    dut.EN_mav_putvalue.value = 1
-    dut.mav_putvalue_instr.value = mav_putvalue_instr
+        # driving the input transaction
+        dut.mav_putvalue_src1.value = mav_putvalue_src1
+        dut.mav_putvalue_src2.value = mav_putvalue_src2
+        dut.mav_putvalue_src3.value = mav_putvalue_src3
+        dut.EN_mav_putvalue.value = 1
+        dut.mav_putvalue_instr.value = mav_putvalue_instr
   
-    yield Timer(1) 
+        yield Timer(1) 
 
-    # obtaining the output
-    dut_output = dut.mav_putvalue.value
+        # obtaining the output
+        dut_output = dut.mav_putvalue.value
 
-    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
-    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+        cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+        cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
     
-    # comparison
-    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
-    assert dut_output == expected_mav_putvalue, error_message
+        # comparison
+        error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+        assert dut_output == expected_mav_putvalue, error_message
+
+
+
